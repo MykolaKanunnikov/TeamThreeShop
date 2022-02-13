3c1eaacd5d661201f90ee43c7546d3b39e850dfa
Nick Eve <kanunnikovmykola@gmail.com>
2022-02-13 17:37:30 +0200

    user and product defined + new lines for each "menu"

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -1,14 +1,24 @@
 package view;
 
+import java.util.Arrays;
+
 public interface Menu {
     void show();
 
     void exit();
 
     default void showItems(String[] items) {
+        clearConsole();
         for (String item : items) {
             System.out.println("-------------");
             System.out.println(item);
         }
+    }
+    static void clearConsole () {
+        char c = '\n';
+        int length = 25;
+        char[] chars = new char[length];
+        Arrays.fill(chars, c);
+        System.out.print(String.valueOf(chars));
     }
 }
